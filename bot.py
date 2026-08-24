ef10a316aa43596d1772b87d675fe67f11e55d37
shariftojiboev9-droid <shariftojiboev9@gmail.com>
2026-08-25 00:23:15 +0500

    Add basic Telegram bot functionality

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+import telebot
+
+TOKEN = os.getenv('TG_TOKEN', '8840551930:AAGX6kaDLpS1APQN1bC6WYWgv6v18x1H4Jk')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я запущен на Render! 🚀")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+print("Бот запущен...")
+bot.infinity_polling()
